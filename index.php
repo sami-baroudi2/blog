dcb504c3c9944c36c0684c032e2191e03d668816
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2021-12-20 12:22:37 +0100

    index

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+
+/* Ligne de commandes GIT 
+
+add : git add [nom du fichier] ou si tu veux push tout le dossier '*' 
+commit : git commit -m 'ecrire le commit ici' 
+push : git push -u origin main 
+
+pull : git pull 
+*/
