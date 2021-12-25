95aff05ccc9b9073c343c253cae36f5aa026b07a
Sami <sami.baroudi@laplateforme.io>
2021-12-26 00:24:34 +0100

    commit

---
+++ PHP/connexion.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="connexion_treatment.php" method="post">
+        <label for="login">Login or Email</label>
+        <input type="text" name="login" required><br><br>
+        <label for="password">Password</label>
+        <input type="password" name="password" required><br><br>
+        <input type="submit" name="BtnCo">
+
+        <p><a class="forgot" href="forgot_password.php">Forgot your password ?</a></p>
+    </form>
+</body>
+</html>
