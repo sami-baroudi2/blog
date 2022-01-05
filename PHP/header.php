83d9711bec14205dcf555cf3ab3441300623cb76
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-01-05 13:29:48 +0100

    Update header.php

--- PHP/header.php
+++ PHP/header.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!-- Page par Jul -->
 <?php
 session_start();
@@ -30,36 +29,3 @@ session_start();
     </nav>
 </body>
 </html>
-=======
-<!-- Page par Jul -->
-<?php
-session_start();
-?>
-<!-- HTML -->
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Header</title>
-</head>
-<body>
-    <!-- Header dynamique -->
-    <nav class="nav bg-dark justify-content-center">
-        <?php
-        if (isset($_SESSION['id']))
-        {
-            echo "<a href='PHP/deconnexion.php' class='nav-link text-light'>Déonnexion</a>
-            <a href='PHP/profil.php' class='nav-link text-light'>Modifier le profil</a>";
-        }
-        else
-        {
-            echo "<a href='PHP/connexion.php' class='nav-link text-light'>Connexion</a>
-            <a href='PHP/inscription.php' class='nav-link text-light'>Inscription</a>";
-        }
-        ?>
-    </nav>
-</body>
-</html>
->>>>>>> a6790694837c85ac42b7dbd7fdd329a39e95b0ff
