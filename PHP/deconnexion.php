ef2335ebf7ec96c53c69ff588b02136f43964b1d
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-01-16 15:42:14 +0100

    Déconnexion
    Page déconnexion

--- PHP/deconnexion.php
+++ PHP/deconnexion.php
@@ -2,6 +2,6 @@
 // Page par Jul.
 session_start(); // On ouvre une session.
 session_destroy(); // On ferme la session.
-header('location: index.php'); // On redirige la personne à l'acceuil si l'utilisateur s'est déconnecté.
+header('location: ../index.php'); // On redirige la personne à l'acceuil si l'utilisateur s'est déconnecté.
 exit;
 ?>
