e44d7046a3e07e3e0d3629ba3ed21f34569b636c
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-01-20 15:11:01 +0100

    Page admin
    Panel d'administration de blog.

---
+++ paneladmin/admin/admin.php
@@ -0,0 +1,16 @@
+<?php
+require_once('configuration.php');
+$users = $bdd->query('SELECT * FROM `utilisateurs` ORDER BY id DESC'); // Je sélectionne les utilisateurs et les membres les plus récents.
+?>
+<!-- Début de HTML. -->
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Panel d'administration du blog.">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSS/blog.css">
+    <title>Admin - blog.</title>
---
+++ paneladmin/index.php
@@ -0,0 +1,3 @@
+<!-- Page par Jul -->
+<?= realpath('admin.php'); // Le chemin.
+?>
