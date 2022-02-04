d90cba7f43317e5502cade02b0dc05080773f2d8
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-02-04 14:13:39 +0100

    Update header.php

--- PHP/header.php
+++ PHP/header.php
@@ -14,16 +14,16 @@ session_start();
     <!-- Header dynamique -->
     <nav class="nav bg-dark justify-content-center">
         <?php
-        if(isset($_SESSION['id']))
+        if(isset($_SESSION['id'])) // Lorsque la personne est connectée, il verra ces 2 sections dans le header.
         {
             echo "<a href='PHP/deconnexion.php' class='nav-link text-light'>Déconnexion</a>
             <a href='PHP/profil.php' class='nav-link text-light'>Modifier le profil</a>";
-            if($_SESSION['droits'] == 1337)
+            if($_SESSION['droits'] == 1337) // Seul l'admin verra cette section dans le header. 👮
             {
                 echo "<a href='PHP/admin.php' class='nav-link text-light'>Panel admin</a>";
             }
         }
-        else
+        else // Lorsque la personne est déconnectée, il verra ces 2 sections dans le header.
         {
             echo "<a href='PHP/connexion.php' class='nav-link text-light'>Connexion</a>
             <a href='PHP/inscription.php' class='nav-link text-light'>Inscription</a>";
