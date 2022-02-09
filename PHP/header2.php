dbed1378f01b9576b198ccff67a20f2363276aa2
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-02-09 10:56:41 +0100

    Add files via upload

---
+++ PHP/header2.php
@@ -0,0 +1,15 @@
+<!-- Page par Jul -->
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Header</title>
+    </head>
+    <body>
+        <nav class="nav bg-dark justify-content-center">
+            <a class='nav-link text-light' href="inscription.php">S'inscrire</a>
+        </nav>
+    </body>
+</html>
