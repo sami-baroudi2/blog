2bb5c7e37b312f94ebcbba24a9dc503661dd6422
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-02-21 15:01:53 +0100

    Add files via upload

--- PHP/header.php
+++ PHP/header.php
@@ -2,33 +2,25 @@
 <?php
 session_start();
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Header</title>
-</head>
-<body>
-    <!-- Header dynamique -->
-    <nav class="nav bg-dark justify-content-center">
-        <?php
-        if(isset($_SESSION['id'])) // Lorsque la personne est connectée, il verra ces 2 sections dans le header.
-        {
-            echo "<a href='PHP/deconnexion.php' class='nav-link text-light'>Déconnexion</a>
-            <a href='PHP/profil.php' class='nav-link text-light'>Modifier le profil</a>";
-            if($_SESSION['droits'] == 1337) // Seul l'admin verra cette section dans le header. 👮
-            {
-                echo "<a href='PHP/admin.php' class='nav-link text-light'>Panel admin</a>";
-            }
-        }
-        else // Lorsque la personne est déconnectée, il verra ces 2 sections dans le header.
-        {
-            echo "<a href='PHP/connexion.php' class='nav-link text-light'>Connexion</a>
-            <a href='PHP/inscription.php' class='nav-link text-light'>Inscription</a>";
-        }    
-        ?>
-    </nav>
-</body>
-</html>
+        <!-- Header dynamique -->
+        <nav class="nav bg-dark justify-content-center">
+            <?php if(isset($_SESSION['id'])) : ?> <!-- Lorsque la personne est connectée, il verra ces 2 sections dans le header. -->
+
+                <a href='PHP/deconnexion.php' class='nav-link text-light'>Déconnexion</a>
+                <a href='PHP/profil.php' class='nav-link text-light'>Modifier le profil</a>
+                <?php if($_SESSION['droits'] == 1337) : ?> <!-- Seul l'admin verra cette section dans le header. 👮 -->
+                    <a href='PHP/admin.php' class='nav-link text-light'>Panel admin</a>
+                <?php endif; ?>
+            <?php else : ?> <!-- Lorsque la personne est déconnectée, il verra ces 2 sections dans le header. -->
+                <a href='PHP/connexion.php' class='nav-link text-light'>Connexion</a>
+                <a href='PHP/inscription.php' class='nav-link text-light'>Inscription</a>
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">Catégorie d'articles</button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="cat1.php">Catégorie 1</a></li>
+                        <li><a class="dropdown-item" href="cat2.php">Catégorie 2</a></li>
+                        <li><a class="dropdown-item" href="cat3.php">Catégorie 3</a></li>
+                    </ul>
+                </div>
+            <?php endif; ?>
+        </nav>
