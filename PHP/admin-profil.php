04dbef854e881cfa19b697518fbad2d07cf20666
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-02-24 13:51:43 +0100

    Update admin-profil.php

--- PHP/admin-profil.php
+++ PHP/admin-profil.php
@@ -35,7 +35,7 @@ $grabID=$_GET['id']; // Il va récupérer l'utilisateur sélectionné.
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> <!-- Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> <!-- Bootstrap -->
-        <link rel="stylesheet" href="CSS/blog.css">
+        <link rel="stylesheet" href="../CSS/blog.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Modifier son profil (Admin) - Blog</title>
     </head>
